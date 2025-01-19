7330db92a4836e50544a283d68274694ee40ca15
Gavin Crump <aussiebimguru@gmail.com>
2025-01-19 18:37:20 +1030

    Additional utilities
    Element extensions, parameter/excel utilities

---
+++ geeWiz/Extensions/Element_Ext.cs
@@ -0,0 +1,204 @@
+﻿// Revit API
+using Autodesk.Revit.UI;
+using View = Autodesk.Revit.DB.View;
+// geeWiz
+using ParameterHelper = geeWiz.Utilities.ParameterHelper;
+
+
+// The class belongs to the extensions namespace
+// Element element.ExtensionMethod()
+namespace geeWiz.Extensions
+{
+    /// <summary>
+    /// Methods of this class generally relate to elements.
+    /// </summary>
+    public static class Element_Ext
+    {
+        /// <summary>
+        /// Constructs a name key based on the class the Element is.
+        /// </summary>
+        /// <param name="element">A Revit Element.</param>
+        /// <param name="includeId">Append the ElementId to the end.</param>
+        /// <returns>A string.</returns>
+        public static string Ext_ToNameKey(this Element element, bool includeId)
+        {
+            // Catch null element
+            if (element is null)
+            {
+                if (includeId)
+                {
+                    return "???: ??? [-1]";
+                }
+                else
+                {
+                    return "???: ???";
+                }
+            }
+            
+            // Default state of values
+            string prefix = "???";
+            string suffix = "???";
+
+            // Check if element is a sheet
+            if (element is ViewSheet sheet)
+            {
+                prefix = "Sheet";
+                suffix = $"{sheet.SheetNumber} - {sheet.Name}";
+            }
+            // Check if element is a viewfamilytype
+            else if (element is ViewFamilyType viewFamilyType)
+            {
+                prefix = viewFamilyType.ViewFamily.ToString();
+                suffix = viewFamilyType.Name;
+            }
+            // Check if element is a view
+            else if (element is View view)
+            {
+                prefix = view.ViewType.ToString();
+                if (view.IsTemplate)
+                {
+                    prefix += " template";
+                }
+                suffix = view.Name;
+            }
+            // Check if element is a family type
+            else if (element is FamilySymbol familySymbol)
+            {
+                prefix = familySymbol.Family.FamilyCategory.Name;
+                suffix = $"{familySymbol.Family.Name} - {familySymbol.Name}";
+            }
+            // Check if element is a family instance
+            else if (element is FamilyInstance familyInstance)
+            {
+                var symbol = familyInstance.Symbol;
+                prefix = symbol.Family.FamilyCategory.Name;
+                suffix = $"{symbol.Family.Name} - {symbol.Name}";
+            }
+            // Otherwise process as an element
+            else
+            {
+                // Try to get category name
+                if (element.Category is Category category)
+                {
+                    prefix = category.Name;
+                }
+                // Otherwise try to get element type name
+                else
+                {
+                    try
+                    {
+                        prefix = element.GetType().Name;
+                    }
+                    catch {; }
+                }
+                
+                // Try to get element name
+                try
+                {
+                    suffix = element.Name;
+                }
+                catch {; }
+            }
+
+            // If we include element id
+            if (includeId && element.Id is ElementId id)
+            {
+                suffix += $" [{id.ToString()}]";
+            }
+
+            // Return the key
+            return $"{prefix}: {suffix}";
+        }
+
+        /// <summary>
+        /// Constructs a parameter helper object to get parameter values.
+        /// </summary>
+        /// <param name="element">A Revit Element.</param>
+        /// <param name="doc">The Document to delete from (optional).</param>
+        /// <returns>A ParameterHelper object.</returns>
+        public static Result Ext_TryToDelete(this Element element, Document doc = null)
+        {
+            // Get the document if null
+            if (doc is null) { doc = element.Document; }
+            
+            // Try to delete the element
+            try
+            {
+                doc.Delete(element.Id);
+                return Result.Succeeded;
+            }
+            catch
+            {
+                return Result.Failed;
+            }
+        }
+
+        /// <summary>
+        /// Constructs a parameter helper object to get parameter values.
+        /// </summary>
+        /// <param name="element">A Revit Element (extended).</param>
+        /// <param name="parameterName">The parameter name to get.</param>
+        /// <returns>A ParameterHelper object.</returns>
+        public static ParameterHelper Ext_GetParameterValueByName(this Element element, string parameterName)
+        {
+            return new ParameterHelper(element, parameterName);
+        }
+
+        /// <summary>
+        /// Gets the value of a text based parameter, if able.
+        /// </summary>
+        /// <param name="element">A Revit Element (extended).</param>
+        /// <param name="parameterName">The parameter name to get.</param>
+        /// <returns>A string.</returns>
+        public static string Ext_GetParameterTextByName(this Element element, string parameterName)
+        {
+            // Early exit on null
+            if (element is null) { return null; }
+
+            // Try to get parameter
+            Parameter parameter = element.LookupParameter(parameterName);
+
+            // Check if parameter is valid
+            if (parameter is null) { return null; }
+
+            // Return string value if parameter is string type
+            if (parameter.StorageType == StorageType.String)
+            {
+                return parameter.AsString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Sets the value of a text based parameter, if able.
+        /// </summary>
+        /// <param name="element">A Revit Element (extended).</param>
+        /// <param name="parameterName">The parameter name to set.</param>
+        /// <param name="value">The string value to set.</param>
+        /// <returns>A Result.</returns>
+        public static Result Ext_SetParameterTextByName(this Element element, string parameterName, string value)
+        {
+            // Early exit on nulls
+            if (element is null || value is null) { return Result.Failed; }
+            
+            // Try to get parameter
+            Parameter parameter = element.LookupParameter(parameterName);
+
+            // Check if parameter is valid
+            if (parameter is null) { return Result.Failed; }
+
+            // Set string value if parameter is string type
+            if (parameter.StorageType == StorageType.String)
+            {
+                parameter.Set(value);
+                return Result.Succeeded;
+            }
+
+            // Return failed if we did not succeed
+            return Result.Failed;
+        }
+    }
+}
---
+++ geeWiz/Utilities/ParameterUtils.cs
@@ -0,0 +1,238 @@
+﻿// Revit API
+using Autodesk.Revit.UI;
+
+// The class belongs to the utilities namespace
+// gPar = geeWiz.Utilities.ParameterUtils
+namespace geeWiz.Utilities
+{
+    /// <summary>
+    /// Methods of this class generally relate to string based operations.
+    /// </summary>
+    public static class ParameterUtils
+    {
+        /// <summary>
+        /// Try to set a parameter using a ParameterHelper object.
+        /// </summary>
+        /// <param name="helper">A ParameterHelper object.</param>
+        /// <returns>A Result.</returns>
+        public static Result Ext_SetParameterValueByName(ParameterHelper helper)
+        {
+            // Check if we should not proceed
+            if (helper.Parameter is null || helper.Element is null)
+            {
+                return Result.Failed;
+            }
+
+            // Set depending on stored value
+            if (helper.StorageType == StorageType.String)
+            {
+                return ParameterHelper.SetString(helper);
+            }
+            else if (helper.StorageType == StorageType.Integer)
+            {
+                return ParameterHelper.SetInteger(helper);
+            }
+            else if (helper.StorageType == StorageType.Double)
+            {
+                return ParameterHelper.SetDouble(helper);
+            }
+            else if (helper.StorageType == StorageType.ElementId)
+            {
+                return ParameterHelper.SetElementId(helper);
+            }
+
+            // Otherwise return failed result
+            return Result.Failed;
+        }
+    }
+    
+    /// <summary>
+    /// A class to assess parameter values in various representations.
+    /// </summary>
+    public class ParameterHelper
+    {
+        // Representations of the parameter
+        public Element Element { get; set; }
+        public Parameter Parameter { get; set; }
+        public StorageType StorageType { get; set; }
+        public int AsInteger { get; set; }
+        public double AsDouble { get; set; }
+        public string AsString { get; set; }
+        public ElementId AsElementId { get; set; }
+
+        // Constructor for get value
+        public ParameterHelper(Element element, string parameterName)
+        {
+            // Store the element
+            this.Element = element;
+
+            // Store the parameter
+            Parameter parameter = element.LookupParameter(parameterName);
+            this.Parameter = parameter;
+
+            // Default values to return (assume nothing found)
+            this.AsString = null;
+            this.AsInteger = 0;
+            this.AsDouble = 0.0;
+            this.AsElementId = ElementId.InvalidElementId;
+            this.StorageType = StorageType.None;
+
+            // Return default value if parameter is none
+            if (parameter is null) { return; }
+            this.StorageType = parameter.StorageType;
+
+            // Work through the various storage types, storing what we can
+            if (this.StorageType == StorageType.String)
+            {
+                if (parameter.AsString() is string value)
+                {
+                    this.AsString = value;
+                }
+            }
+            else if (this.StorageType == StorageType.Integer)
+            {
+                if (parameter.AsInteger() is int value)
+                {
+                    this.AsInteger = value;
+                    this.AsString = value.ToString();
+                }
+            }
+            else if (this.StorageType == StorageType.Double)
+            {
+                if (parameter.AsDouble() is double value)
+                {
+                    this.AsDouble = value;
+                    this.AsInteger = (int)value;
+                    this.AsString = value.ToString();
+                }
+            }
+            else if (this.StorageType == StorageType.ElementId)
+            {
+                if (parameter.AsElementId() is ElementId value)
+                {
+                    this.AsElementId = value;
+                    this.AsInteger = value.IntegerValue;
+                    this.AsString = value.ToString();
+                    this.AsDouble = (double)value.IntegerValue;
+
+                }
+            }
+        }
+
+        // Constructor to set as string
+        public ParameterHelper(Element element, string parameterName, string value)
+        {
+            // Store the element
+            this.Element = element;
+
+            // Store the parameter
+            Parameter parameter = element.LookupParameter(parameterName);
+            this.Parameter = parameter;
+
+            // Store the type and value
+            this.StorageType = StorageType.String;
+            this.AsString = value;
+        }
+
+        // Method to set string value
+        public static Result SetString(ParameterHelper helper)
+        {
+            try
+            {
+                helper.Parameter.Set(helper.AsString);
+                return Result.Succeeded;
+            }
+            catch
+            {
+                return Result.Failed;
+            }
+        }
+
+        // Constructor to set as integer
+        public ParameterHelper(Element element, string parameterName, int value)
+        {
+            // Store the element
+            this.Element = element;
+
+            // Store the parameter
+            Parameter parameter = element.LookupParameter(parameterName);
+            this.Parameter = parameter;
+
+            // Store the type and value
+            this.StorageType = StorageType.Integer;
+            this.AsInteger = value;
+        }
+
+        // Method to set integer value
+        public static Result SetInteger(ParameterHelper helper)
+        {
+            try
+            {
+                helper.Parameter.Set(helper.AsInteger);
+                return Result.Succeeded;
+            }
+            catch
+            {
+                return Result.Failed;
+            }
+        }
+
+        // Constructor to set as double
+        public ParameterHelper(Element element, string parameterName, double value)
+        {
+            // Store the element
+            this.Element = element;
+
+            // Store the parameter
+            Parameter parameter = element.LookupParameter(parameterName);
+            this.Parameter = parameter;
+
+            // Store the type and value
+            this.StorageType = StorageType.Double;
+            this.AsDouble = value;
+        }
+
+        // Method to set double value
+        public static Result SetDouble(ParameterHelper helper)
+        {
+            try
+            {
+                helper.Parameter.Set(helper.AsDouble);
+                return Result.Succeeded;
+            }
+            catch
+            {
+                return Result.Failed;
+            }
+        }
+
+        // Constructor to set as element Id
+        public ParameterHelper(Element element, string parameterName, ElementId value)
+        {
+            // Store the element
+            this.Element = element;
+
+            // Store the parameter
+            Parameter parameter = element.LookupParameter(parameterName);
+            this.Parameter = parameter;
+
+            // Store the type and value
+            this.StorageType = StorageType.ElementId;
+            this.AsElementId = value;
+        }
+
+        // Method to set ElementId
+        public static Result SetElementId(ParameterHelper helper)
+        {
+            try
+            {
+                helper.Parameter.Set(helper.AsElementId);
+                return Result.Succeeded;
+            }
+            catch
+            {
+                return Result.Failed;
+            }
+        }
+    }
+}
